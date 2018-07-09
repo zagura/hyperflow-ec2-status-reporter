29cddcafa55c015509431290a87a83ef185b4e00
krystian pawlik <krysp89@gmail.com>
2018-07-09 23:09:56 +0200

    cpu and memory reporting

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 #!/usr/bin/env node
 
 var METRIC_COLLECTOR = process.env.METRIC_COLLECTOR ? process.env.METRIC_COLLECTOR : 'http://localhost:8086/hyperflow_tests';
-
+var INTERFACE = process.env.INTERFACE ? process.env.INTERFACE : 'eth0';
 
 
 var os = require('os');
-
-console.log(os.cpus());
-console.log(os.totalmem());
-console.log(os.freemem())
+const si = require('systeminformation');
 
 var os_utils = require('os-utils');
 var AWS = require('aws-sdk');
 const Influx = require('influxdb-nodejs');
-
-os_utils.cpuUsage(function(v){
-    console.log( 'CPU Usage (%): ' + v );
-});
 
 var meta  = new AWS.MetadataService();
 
@@ -38,13 +31,27 @@ function writeDataToDatabase(metric, data,tag)
 function collectUsage(instance_id)
 {
     os_utils.cpuUsage(function(v){
-        console.log( 'CPU Usage (%): ' + v );
+        //console.log( 'CPU Usage (%): ' + v );
         writeDataToDatabase("hyperflow_cpu_usage_ec2",{ cpu_usage:v},{ec2_incance_id: instance_id});
     });
 
-    var freemem=os_utils.freememPercentage();
-    console.log('Free Memory (%): '+ freemem);
-    writeDataToDatabase("hyperflow_memory_usage_ec2",{ free_memory:freemem},{ec2_incance_id: instance_id});
+    si.mem(function(data) {
+        //console.log('Memory used:');
+        var used_memory=data.used/1024;
+        //console.log(data.used/1024);
+        writeDataToDatabase("hyperflow_memory_usage_ec2",{ used_memory:used_memory},{ec2_incance_id: instance_id});
+    });
+
+    si.networkStats(INTERFACE,function(data){
+        //console.log('eth0 used:');
+        console.log(data);
+        if(data.rx_sec!=-1)
+        {
+            writeDataToDatabase("hyperflow_connection_received",{ received_bytes_per_s:data.rx_sec},{ec2_incance_id: instance_id});
+            writeDataToDatabase("hyperflow_connection_transferred",{ transferred_bytes_per_s:data.tx_sec},{ec2_incance_id: instance_id});
+        }
+    });
+    
 }
 
 
